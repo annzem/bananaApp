e97c3d1bec20a09568bdb5057b02e0047097be99
anna <annzemlinsky@gmail.com>
2022-03-16 20:21:29 +0300

    remove some jq

--- src/main/resources/static/static/js/script.js
+++ src/main/resources/static/static/js/script.js
@@ -11,3 +11,14 @@ Handlebars.registerHelper("more", function(a, b, options) {
     return a > b;
 });
 
+function getCsrf() {
+    let metas = document.getElementsByTagName("meta");
+    let token;
+    for (let i = 0; i < metas.length; i++) {
+        if (metas[i].name == "_csrf"){
+            token = metas[i].content;
+            break;
+        }
+    }
+    return token;
+}
